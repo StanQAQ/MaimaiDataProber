2a91ea1c4ff9047e9fda0a0776344d97cf199f7d
StanQ <jb6806973@gmail.com>
2025-06-03 19:53:21 +0800

    added background & beautify layout

--- start.bat
+++ start.bat
@@ -3,13 +3,13 @@
 :: 启动 Node.js 服务器
 start cmd /k "node proxy_server.js"
 
-:: 等待 0.5 秒钟，确保服务器启动
+:: 等待 1 秒，确保服务器启动
 timeout /t 1
 
 :: 启动 http 服务器
 start cmd /k "python -m http.server 8000"
 
-:: 等待 0.5 秒钟，确保服务器启动
+:: 等待 1 秒，确保服务器启动
 timeout /t 1
 
 :: 打开浏览器并加载 api_fetch_test.html 页面
